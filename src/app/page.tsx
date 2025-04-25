aa60f42baabb986a1b93feb12fb1ec5c4b10c101
vickydev03 <vickysinghdev03@gmail.com>
2025-04-25 12:57:58 +0530

    02:customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 import React from "react";
 
 function Home() {
   return (
-    <div>
-      <Button variant={"destructive"}> hello world</Button>
+    <div className="p-10 flex flex-col  gap-5">
+      <Button variant={"elevated"}> hello world</Button>
+      <Input placeholder="enter the chat" />
+      <Checkbox />
+      <Progress value={50} />
       <p className="text-red-300">this is home page</p>
     </div>
   );
