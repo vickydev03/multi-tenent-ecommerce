a0025e48a520a940c63fca0db2dca9cc711bcd11
vickydev03 <vickysinghdev03@gmail.com>
2025-04-26 17:00:42 +0530

    integrated payload cms

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -21,7 +21,7 @@ function Progress({
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-pink-400 h-2 h-full w-full flex-1 transition-all"
+        className="bg-pink-400 h-full  w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
