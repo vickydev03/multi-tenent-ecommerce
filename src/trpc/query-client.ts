6b697d3cf9d6a22e22c04c8b94e713d675e9d5e5
vickydev03 <vickysinghdev03@gmail.com>
2025-04-29 02:13:52 +0530

    fix trpc.0

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -10,13 +10,13 @@ import {
           staleTime: 30 * 1000,
         },
         dehydrate: {
-          // serializeData: superjson.serialize,
+          serializeData: superjson.serialize,
           shouldDehydrateQuery: (query) =>
             defaultShouldDehydrateQuery(query) ||
             query.state.status === 'pending',
         },
         hydrate: {
-          // deserializeData: superjson.deserialize,
+          deserializeData: superjson.deserialize,
         },
       },
     });
